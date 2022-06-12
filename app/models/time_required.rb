c765585284a09d7c39681e6cba5892597bbeb316
na05yuki <pempem.kmkm@gmail.com>
2022-06-13 00:25:24 +0900

    調理時間のプルダウン項目記述

---
+++ app/models/time_required.rb
@@ -0,0 +1,14 @@
+class TimeRequired < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '5分以内' },
+    { id: 3, name: '10分以内' },
+    { id: 4, name: '20分以内' },
+    { id: 5, name: '30分以内' },
+    { id: 6, name: '30分以上' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :recipes
+
+  end
