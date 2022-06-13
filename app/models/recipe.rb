cf7d233101d105957549acf6bb3e285d1f80cd7f
na05yuki <pempem.kmkm@gmail.com>
2022-06-13 23:51:22 +0900

    recipeモデルのアソシエーション、バリデーション設定

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,20 @@
 class Recipe < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :time_required
+  belongs_to :number_of_people
+  belongs_to :category
+
+  belongs_to :user
+  has_many :comments
+
+  with_options presence: true do
+    validates :title
+    validates :explanation
+
+    with_options numericality: { other_than: 1, message: 'を選択してください' } do
+      validates :time_required_id
+      validates :number_of_people_id
+      validates :category_id
+    end
+  end
 end
