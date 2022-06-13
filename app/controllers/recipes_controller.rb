3aac709c6c60b638a81644a354b405965c939512
na05yuki <pempem.kmkm@gmail.com>
2022-06-14 00:44:39 +0900

    コントローラーに新規投稿機能実装

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,6 +4,21 @@ class RecipesController < ApplicationController
   end
 
   def new
+    @recipe = Recipe.new
   end
-  
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to root_path
+    else
+      render :new
+   end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title,:explanation,:time_required_id,:number_of_people_id,:category_id)
+  end
 end
