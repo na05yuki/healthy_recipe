511c10e78d08a10a19eca108f49e814d71a0fa7e
na05yuki <pempem.kmkm@gmail.com>
2022-06-22 22:52:27 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   
   root to: 'recipes#index'
-  get '/recipes/category', to: 'recipes#category'
-  resources :recipes
+  resources :recipes do
+    collection do
+      get 'category'
+    end
+  end
 end
