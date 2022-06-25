a7fd926d4fd3f2de34e46d45ce8a916bb69aaa1b
na05yuki <pempem.kmkm@gmail.com>
2022-06-25 11:53:39 +0900

    Revert "検索機能のルーティング再設定"
    This reverts commit d4d822f6ea2893a1a995d56fbec4dfea7e61ee31.

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   
   root to: 'recipes#index'
-  resources :recipes do
-    collection do
-      get 'category'
-    end
-  end
+  resources :recipes
 end
