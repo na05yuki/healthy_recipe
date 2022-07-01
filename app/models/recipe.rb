e8401548daef6c5973857ba294fa58c2ae1c2fc2
na05yuki <pempem.kmkm@gmail.com>
2022-07-02 07:12:56 +0900

    ユーザー、レシピが削除されたらお気に入りも削除されるように設定

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,7 +5,7 @@ class Recipe < ApplicationRecord
   belongs_to :category
 
   belongs_to :user
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   has_one_attached :image
 
   with_options presence: true do
