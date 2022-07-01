98a67ca528cc4f37c827a917efd0e1d2492129e6
na05yuki <pempem.kmkm@gmail.com>
2022-07-01 23:25:28 +0900

    アソシエーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :recipes
-  has_many :comments
+  has_many :favorites
 
   validates :nickname, presence: true, uniqueness: true
 
